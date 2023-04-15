04b4d7085744bf24d00eaaa2f85092a03bcce4c9
Bruce Lee <brucehlee@yahoo.ca>
2023-04-15 11:03:23 -0400

    LighthBnB Web Boilerplate

--- 1_queries/most_visited_cities.sql
+++ 1_queries/most_visited_cities.sql
@@ -1,10 +1,3 @@
--- Our product managers want a query to see a list of the most visited cities.
-
--- Instruction
--- Get a list of the most visited cities.
-
--- Select the name of the city and the number of reservations for that city.
--- Order the results from highest number of reservations to lowest number of reservations.
 
 SELECT properties.city, count(reservations) as total_reservations
 FROM reservations
